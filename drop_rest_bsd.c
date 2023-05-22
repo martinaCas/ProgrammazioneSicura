df5f99369ebba76ccfe36df8e1edb2c05a11da24
martinaCas <103025298+martinaCas@users.noreply.github.com>
2023-05-22 17:59:26 +0200

    Add files via upload

---
+++ drop_rest_bsd.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    uid_t uid = getuid();
+    uid_t euid = geteuid();
+
+
+    printf( "Prima di setreuid(euid, uid) : RUID del processo = %d\n", uid);
+    printf( "Prima di setreuid(euid, uid) : EUID del processo = %d\n", euid);
+
+    if(setreuid(euid, uid) == -1){
+            printf( "Non sono riuscito a scambiare UID <--> EUID .\n");
+            exit(1);
+    }
+
+    uid = getuid();
+    euid = geteuid();
+
+    printf( "Dopo setreuid(euid, uid)  : RUID del processo = %d\n", uid);
+    printf( "Dopo setreuid(euid, uid)  : EUID del processo = %d\n", euid);
+
+    if(setreuid(euid, uid) == -1){
+        printf( "Non sono riuscito a scambiare UID <--> EUID .\n");
+            exit(1);
+    }
+
+    uid = getuid();
+    euid= geteuid();
+
+    printf( "Dopo setreuid(euid, uid)  : RUID del processo = %d\n", uid);
+    printf( "Dopo setreuid(euid, uid)  : EUID del processo = %d\n", euid);
+
+
+    exit(0);
+}
